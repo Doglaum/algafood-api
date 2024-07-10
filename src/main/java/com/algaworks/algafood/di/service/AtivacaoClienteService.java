63e29e1f3d6d6e9c3d59e9296c1c767fdd3c6365
douglas.martins <heredouglas@gmail.com>
2024-07-10 19:53:54 -0300

    #F022 ESR - 02.14 - Conhecendo os pontos de injeção e a anotação @Autowired

--- src/main/java/com/algaworks/algafood/di/service/AtivacaoClienteService.java
+++ src/main/java/com/algaworks/algafood/di/service/AtivacaoClienteService.java
@@ -3,14 +3,25 @@ package com.algaworks.algafood.di.service;
 
 import com.algaworks.algafood.di.modelo.Cliente;
 import com.algaworks.algafood.di.notificacao.Notificador;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AtivacaoClienteService {
 
+    //Inejetar 1
+    @Autowired
     private Notificador notificador;
 
-    public AtivacaoClienteService(Notificador notificador) {
+    //Injetar 2
+    @Autowired
+    public AtivacaoClienteService (Notificador notificador) {
+        this.notificador = notificador;
+    }
+
+    //Injetar 3
+    @Autowired
+    public void setNotificador(Notificador notificador) {
         this.notificador = notificador;
     }
 
