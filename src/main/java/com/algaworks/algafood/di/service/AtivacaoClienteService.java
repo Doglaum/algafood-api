c7c59f447576126005010644b1dc1d7b3836fc1c
douglas.martins <heredouglas@gmail.com>
2024-07-11 22:27:05 -0300

    #F030 ESR - 02.22 - Publicando e consumindo eventos customizados

--- src/main/java/com/algaworks/algafood/di/service/AtivacaoClienteService.java
+++ src/main/java/com/algaworks/algafood/di/service/AtivacaoClienteService.java
@@ -2,30 +2,18 @@ package com.algaworks.algafood.di.service;
 
 
 import com.algaworks.algafood.di.modelo.Cliente;
-import com.algaworks.algafood.di.notificacao.NivelUrgencia;
-import com.algaworks.algafood.di.notificacao.Notificador;
-import com.algaworks.algafood.di.notificacao.TipoDoNotificador;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.stereotype.Component;
 
-//@Component
+@Component
 public class AtivacaoClienteService {
 
-    @TipoDoNotificador(NivelUrgencia.SEM_URGENCIA)
     @Autowired
-    private Notificador notificador;
-
-    //@PostConstruct
-    public void init() {
-        System.out.println("init");
-    }
-
-    //@PreDestroy
-    public void destroy() {
-        System.out.println("destroy");
-    }
+    private ApplicationEventPublisher eventPublisher;
 
     public void ativar(Cliente cliente) {
         cliente.ativar();
-        notificador.notificar(cliente, "Seu cadsatro no sistema está ativo");
+        eventPublisher.publishEvent(new ClienteAtivadoEvent(cliente));
     }
 }
