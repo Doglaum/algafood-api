7837bc03f1a637bb1c696e989fe308bead813941
douglas.martins <heredouglas@gmail.com>
2024-07-11 22:45:04 -0300

    #F033 ESR - 02.25 - Criando e acessando propriedades customizadas com @Value

--- src/main/java/com/algaworks/algafood/di/notificacao/NotificadorEmail.java
+++ src/main/java/com/algaworks/algafood/di/notificacao/NotificadorEmail.java
@@ -1,6 +1,7 @@
 package com.algaworks.algafood.di.notificacao;
 
 import com.algaworks.algafood.di.modelo.Cliente;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
@@ -9,12 +10,19 @@ import org.springframework.stereotype.Component;
 @Component
 public class NotificadorEmail implements Notificador {
 
+    @Value("${notificador.email.host-servidor}")
+    private String host;
+    @Value("${notificador.email.porta-servidor}")
+    private Integer porta;
+
     public NotificadorEmail() {
         System.out.println("Notificador email real");
     }
 
     @Override
     public void notificar(Cliente cliente, String mensagem) {
+        System.out.println(host);
+        System.out.println(porta.toString());
         System.out.printf("Notificando %s através do e-mail %s: %s\n",
                 cliente.getNome(), cliente.getEmail(), mensagem);
     }
