3aa2f4ec1e1eb3dd65ec11c46d26945f11a3615c
douglas.martins <heredouglas@gmail.com>
2024-07-21 16:05:36 -0300

    #F061 ESR - 03.19 - Propriedade nullable de @Column e @JoinColumn

--- src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
+++ src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
@@ -2,10 +2,7 @@ package com.algaworks.algafood.domain.model;
 
 import lombok.*;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Data
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
@@ -17,6 +14,8 @@ public class Cozinha {
     @EqualsAndHashCode.Include
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false)
     private String nome;
 
     public Cozinha(String nome) {
