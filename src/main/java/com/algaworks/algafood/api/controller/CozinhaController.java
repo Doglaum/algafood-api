2db94e6136d8e4a9ab9ad24d8103ef938bf32c22
douglas.martins <heredouglas@gmail.com>
2024-08-06 22:19:24 -0300

    #F092 ESR - 04.25 - Modelando e implementando a atualização de recursos com PUT

--- src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
+++ src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
@@ -3,6 +3,7 @@ package com.algaworks.algafood.api.controller;
 import com.algaworks.algafood.api.model.CozinhasXmlWrapper;
 import com.algaworks.algafood.domain.model.Cozinha;
 import com.algaworks.algafood.domain.repository.CozinhaRepository;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -23,7 +24,7 @@ public class CozinhaController {
         return cozinhaRepository.listar();
     }
 
-    @GetMapping (value = "/listar", produces = MediaType.APPLICATION_XML_VALUE)
+    @GetMapping(value = "/listar", produces = MediaType.APPLICATION_XML_VALUE)
     public CozinhasXmlWrapper listarXml() {
         //return cozinhaRepository.listar();
         return new CozinhasXmlWrapper(cozinhaRepository.listar());
@@ -45,5 +46,14 @@ public class CozinhaController {
         return cozinhaRepository.salvar(cozinha);
     }
 
-
+    @PutMapping("/{cozinhaId}")
+    public ResponseEntity<Cozinha> atualizar(@PathVariable Long cozinhaId, @RequestBody Cozinha cozinha) {
+        Cozinha cozinhaAtual = cozinhaRepository.buscar(cozinhaId);
+        if(cozinhaAtual != null) {
+            BeanUtils.copyProperties(cozinha, cozinhaAtual, "id");
+            cozinhaAtual = cozinhaRepository.salvar(cozinhaAtual);
+            return ResponseEntity.ok(cozinhaAtual);
+        }
+        return ResponseEntity.notFound().build();
+    }
 }
